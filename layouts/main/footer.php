f0bb9b7c72b38958b7dc0be5192da3691078c792
brandon-del <60769731+brandon-del@users.noreply.github.com>
2021-08-18 19:18:17 +0300

    Delete layouts/main directory

--- layouts/main/footer.php
+++
@@ -1,6 +0,0 @@
-<script src="./js/bootstrap.bundle.min.js"
-    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
-</script>
-</body>
-
-</html>
